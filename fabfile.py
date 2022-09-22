c31f6085e390d7029d5b87be00028b3145a5a7d1
naseemr <naseemra@gmail.com>
2022-09-22 09:34:29 -0500

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -11,13 +11,26 @@ svrlist4 = ['IP1','IP2','IP3','IP4',  ..,'IPN']
 
 @hosts(svrlist1)
 def svrlist1tailf():
+    try:
         sudo(' tail -f /var/log/messages')
+    except KeyboardInterrupt:
+        print("Stopped")
 @hosts(svrlist2)
 def svrlist2tailf():
+    try:
         sudo(' tail -f /var/log/messages')
+    except KeyboardInterrupt:
+        print("Stopped")
+
 @hosts(svrlist3)
 def svrlist3tailf():
+    try:
         sudo(' tail -f /var/log/messages')
+    except KeyboardInterrupt:
+        print("Stopped")
 @hosts(svrlist4)
 def svrlist4tailf():
+    try:
         sudo(' tail -f /var/log/messages')
+    except KeyboardInterrupt:
+        print("Stopped")
